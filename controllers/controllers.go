b845bd49a6b2efcd6314a411ad2a65cb9b70df99
adriannylelis <adrianny.lelis@gmail.com>
2024-06-20 23:01:21 -0400

    feat: db connection

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
+	"github.com/adriannylelis/persona-go-api/database"
 	"github.com/adriannylelis/persona-go-api/models"
 	"github.com/gorilla/mux"
 )
@@ -16,7 +16,9 @@ func Home(w http.ResponseWriter, r *http.Request)  {
 }
 
 func GetPersonalidades(w http.ResponseWriter, r *http.Request)  {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
 	
 }
 
@@ -24,10 +26,7 @@ func GetPersonalidade(w http.ResponseWriter, r *http.Request)  {
 	vars := mux.Vars(r)
 	id:= vars["id"]
 
-	for _, personalidade := range models.Personalidades{
-		if strconv.Itoa(personalidade.ID) == id {
-			json.NewEncoder(w).Encode(personalidade)
-			return
-		}
-	}
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	database "github.com/adriannylelis/persona-go-api/Database"
 	"github.com/adriannylelis/persona-go-api/models"
 	"github.com/adriannylelis/persona-go-api/routes"
 )
@@ -12,7 +13,7 @@ func main()  {
 models.Personalidades =[]models.Personalidade{
 	{Nome: "Nome 1", Descricao: "Descricao 1"},
 }
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando aplicacao...")
 	routes.HandleRequest()
 }
